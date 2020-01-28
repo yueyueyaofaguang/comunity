123b57f4e7015c39e16f94a88146da0b6b64954e
weiwenyue <15961663166@163.com>
2020-01-28 22:14:25 +0800

    引入bootstrape&创建index.html

--- src/main/java/life/majiang/comunity/comunity/controller/IndexController.java
+++ src/main/java/life/majiang/comunity/comunity/controller/IndexController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name",required = false,defaultValue = "World")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
+    //不输入的时候
 }
